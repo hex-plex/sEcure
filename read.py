1db2eaac6dd350a5d36faa57e09945b8c3fc60a7
hex-plex <56990337+hex-plex@users.noreply.github.com>
2020-04-03 18:26:32 +0530

    This makes it well organized and to debug

--- read.py
+++ read.py
@@ -2,7 +2,10 @@ if __name__=="__main__":
     exit()
 else:
     import serial as sl
-    ser=sl.Serial("ttyUSB0",9600)
-    a=ser.read()
-    if(a.find('!')+1):
-        j=(a.split('!')[-1].split('\')[0])
+    
+    def read(port):
+        ser=sl.Serial(port,9600)
+        a=ser.read()
+        if(a.find('!')+1):
+            j=(a.split('!')[-1].split('\')[0])
+        return j
